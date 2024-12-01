113e48ce771010310aa74a09500efffa738e4a49
Rei Sikk <rei@hable.ee>
2024-12-01 18:16:41 +0200

    Single event page header

--- src/pages/event/[event].js
+++ src/pages/event/[event].js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { createClient } from '@/utils/supabase/component'
 import styles from './SingleEventPage.module.css'
-
+import classNames from 'classnames'
+import placehold_image from '../../../public/assets/landing_place.webp'
+import Image from 'next/image'
+import { format } from 'date-fns';
 
 
 export async function getServerSideProps({ params }) {
@@ -24,6 +27,10 @@ export async function getServerSideProps({ params }) {
     } 
 
 
+  const formatTime = (timeString) => {
+      return format(new Date(timeString), 'HH:mm');
+    };
+
 function EventPage ({ event }) {
     console.log(event, "event")
 if (!event) return <div>Loading...</div>
@@ -31,16 +38,31 @@ if (!event) return <div>Loading...</div>
   return (
     <>
         <header className={styles.eventHeader}>
-            <h1 className={styles.eventHeader__title}>{event.title}</h1>
+          <div className={classNames(styles.eventHeader__wrap, styles.container)}>
+            <div className={styles.eventHeader__media}>
+              <Image src={placehold_image} alt={event.title} className={styles.eventHeader__img} />
+              <div className={styles.eventHeader__meta}>
+                <p className={styles.eventHeader__date}>December 12th, Thursday</p>
+                <p className={styles.eventHeader_time}>
+                {formatTime(event.start_time)} until {formatTime(event.end_time)}
+                </p>
+                <p className={styles.eventHeader__location}>{event.location}</p>
+                <div className={styles.eventHeader__gradient}>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main className={styles.eventMain}>
+            <section>
+            {/* <h1 className={styles.eventHeader__title}>{event.title}</h1>
             <img src={event.image} alt={event.title} className={styles.eventHeader__img} />
             <div className={styles.eventDetails}>
                 <h2>Details</h2>
                 <p className={styles.eventHeader__text}>{event.cost ? `Cost: ${event.cost}` : "The event is free" }</p>
                 <p className={styles.eventHeader__text}>{event.description}</p>
-            </div>
-        </header>
-        <main className={styles.eventMain}>
-            <section>
+            </div> */}
             </section>
         </main>
     </>
