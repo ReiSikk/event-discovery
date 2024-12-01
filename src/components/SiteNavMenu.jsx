a108c2377c381998f0b7e73a70364f027d66a9d7
Rei Sikk <rei@hable.ee>
2024-12-01 22:15:55 +0200

    Single events layout and styling

--- src/components/SiteNavMenu.jsx
+++ src/components/SiteNavMenu.jsx
@@ -19,32 +19,22 @@ const SiteNavMenu = () => {
 							<li style={{ gridRow: "span 3" }}>
 								<NavigationMenu.Link asChild>
 									<a className={styles.Callout} href="/">
-										<svg
-											aria-hidden
-											width="38"
-											height="38"
-											viewBox="0 0 25 25"
-											fill="white"
-										>
-											<path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
-											<path d="M12 0H4V8H12V0Z"></path>
-											<path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
-										</svg>
+										<h1>LEIA</h1>
 										<p className={styles.CalloutText}>
-											Browse events & activities on a map.
+											Discover events tailored to your interests
 										</p>
 									</a>
 								</NavigationMenu.Link>
 							</li>
 
-							<ListItem href="https://stitches.dev/" title="Local">
-								CSS-in-JS with best-in-class developer experience.
+							<ListItem href="/home" title="Browse">
+								Browse and explore events.
 							</ListItem>
-							<ListItem href="/colors" title="Fitness">
+							<ListItem href="/home" title="Events near you">
 								Beautiful, thought-out palettes with auto dark mode.
 							</ListItem>
-							<ListItem href="https://icons.radix-ui.com/" title="Enterntainment">
-								A crisp set of 15x15 icons, balanced and consistent.
+							<ListItem href="/home?popular" title="Most popular events">
+								Most sought after events right now
 							</ListItem>
 						</ul>
 					</NavigationMenu.Content>
--- src/pages/event/[event].js
+++ src/pages/event/[event].js
@@ -5,6 +5,7 @@ import classNames from 'classnames'
 import placehold_image from '../../../public/assets/landing_place.webp'
 import Image from 'next/image'
 import { format } from 'date-fns';
+import { CalendarClock, MapPin } from 'lucide-react'
 
 
 export async function getServerSideProps({ params }) {
@@ -55,14 +56,41 @@ if (!event) return <div>Loading...</div>
           </div>
         </header>
         <main className={styles.eventMain}>
-            <section>
-            {/* <h1 className={styles.eventHeader__title}>{event.title}</h1>
-            <img src={event.image} alt={event.title} className={styles.eventHeader__img} />
-            <div className={styles.eventDetails}>
-                <h2>Details</h2>
-                <p className={styles.eventHeader__text}>{event.cost ? `Cost: ${event.cost}` : "The event is free" }</p>
-                <p className={styles.eventHeader__text}>{event.description}</p>
-            </div> */}
+            <section className={classNames(styles.eventContent, styles.container)}>
+              <div className={styles.contentLeft}>
+                <p className={styles.contentLeft__lead}>What?</p>
+                <h1 className={styles.contentLeft__title}>{event.title}</h1>
+                <p className={styles.contentLeft__location}></p>
+                <p className={styles.contentLeft__text}>{event.description}</p>
+                <p className={styles.contentLeft__cost}>{event.cost ? `Cost: ${event.cost}` : "This event is free" }</p>
+              </div>
+              <div className={styles.contentRight}>
+                <div className={styles.contentRight__top}>
+                  <div className={styles.contentRight__item}>
+                    <h3 className={styles.contentRight__title}><CalendarClock />Time & Date</h3>
+                    <p className={styles.contentRight__subtitle}>December 12</p>
+                    <p className={styles.contentRight__text}>2024</p>
+                    <a href="#" className={styles.contentRight__link} >Mark in my calendar</a>
+                  </div>
+                  <div className={styles.contentRight__item}>
+                    <h3 className={styles.contentRight__title}><MapPin />{event.location}</h3>
+                    <p className={styles.contentRight__subtitle}>Name of the place</p>
+                    <p className={styles.contentRight__text}>Exact address here</p>
+                    <a href="#" className={styles.contentRight__link} >Show on the map</a>
+                  </div>
+                </div>
+                <div className={styles.contentStats}>
+                  <div className={styles.contentStats__top}>
+                    <h3 className={styles.contentStats__title}>NB!</h3>
+                    <p className={styles.contentStats__date}>A piece of important information for th event goers</p>
+                  </div>
+                  <div className={styles.contentStats__bottom}>
+                    <h3 className={styles.contentStats__number}>15%</h3>
+                    <h3 className={styles.contentStats__lead}>Off our entire brunch menu for attendees</h3>
+                    <h3 className={styles.contentStats__text}>Some text describing the offer here</h3>
+                  </div>
+                </div>
+              </div>
             </section>
         </main>
     </>
